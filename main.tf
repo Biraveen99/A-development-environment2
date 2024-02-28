68ecde9f78b44546de7612860f7cab744452ed62
Biraveen99 <biraveen44@gmail.com>
2024-02-28 16:57:48 +0100

    endretpåmainfortest

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+module "Development_server_emacs_jed_git" {
+    source = "./Development_server_emacs_jed_git/DevelopmentServerproject1.tf" #2stk_idenne
+  }
+
 module "Docker_server2" {
     source = "./Docker_server2"
   }
+
+
+module "Storage_server_GlusterFS1" {
+    source = "./Storage_server_GlusterFS/Gluster1/Gluster1project1.tf"
+  }
+
+module "Storage_server_GlusterFS1" {
+    source = "./Storage_server_GlusterFS/Gluster2/Gluster2project2.tf"
+  }
+
