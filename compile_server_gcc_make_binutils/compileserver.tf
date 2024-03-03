2191803c89f6e46e886cb46cad4d4bb38c6b6749
Biraveen99 <biraveen44@gmail.com>
2024-03-03 15:16:36 +0100

    compileserverchanges

--- compile_server_gcc_make_binutils/compileserver.tf
+++ compile_server_gcc_make_binutils/compileserver.tf
@@ -1,22 +1,22 @@
 terraform {
   required_providers {
-   openstack = {
-    source = "terraform-provider-openstack/openstack"
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
   }
- }
 }
 
 provider "openstack" {
-  cloud = "openstack" # defined in /Users/biraveennedunchelian/.config/openstack/clouds.yaml
+  cloud = "openstack" # Update this path if necessary
 }
 
-resource "openstack_compute_instance_v2" "Compile_server" {
-  count             = 2 # Creates two instances
-  name              = "compile_server-${count.index}"
-  image_name        = "ubuntu-22.04-LTS"
-  flavor_name       = "css.1c1r.10g" # Ensure this matches a single CPU configuration in your OpenStack
-  key_pair          = "masterKey"
-  security_groups   = ["default"]
+resource "openstack_compute_instance_v2" "compile_server" {
+  count            = 2 # Creates two instances
+  name             = "compile-server-${count.index}"
+  image_name       = "ubuntu-22.04-LTS"
+  flavor_name      = "css.1c1r.10g" # Ensure this matches your desired configuration
+  key_pair         = "masterKey"
+  security_groups  = ["default"]
 
   network {
     name = "acit"
@@ -29,23 +29,18 @@ resource "openstack_compute_instance_v2" "Compile_server" {
     host        = self.access_ip_v4
   }
   
-  
-
-    provisioner "remote-exec" {
-        inline = [
-        "sleep 20",
-        "sudo apt-get update",
-        "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gcc", 
-        "if [ -f /var/run/reboot-required ]; then sudo shutdown -r +1 'Scheduled reboot after GCC installation'; fi",
-        "sudo apt install -y make",
-        "sudo apt install -y binutils",
-
-        ]
-    }
-
-
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 20",
+      "sudo apt-get update",
+      "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y gcc make binutils",
+      # Reboot logic if required by GCC installation
+      "if [ -f /var/run/reboot-required ]; then sudo shutdown -r +1 'Scheduled reboot after GCC installation'; fi",
+    ]
+  }
 }
 
-output "compile_server_server_ips" {
-  value = openstack_compute_instance_v2.Compile_server.*.access_ip_v4
+output "compile_server_ips" {
+  value = openstack_compute_instance_v2.compile_server.*.access_ip_v4
 }
+
